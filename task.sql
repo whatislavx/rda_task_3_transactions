bcd4f4b07ed115bc03405e76d0937b3d0c5c1ec7
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2025-11-12 20:07:13 +0200

    Solution v3

--- task.sql
+++ task.sql
@@ -1,9 +1,9 @@
 USE ShopDB; 
+ 
+START TRANSACTION; 
 
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
- 
-START TRANSACTION; 
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (LAST_INSERT_ID(), 1, 1);
